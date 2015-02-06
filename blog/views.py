23065697bf982091e7331c2f4dc540756c7909a0
Anton Kuznetsov <isnifer@gmail.com>
2015-02-06 03:01:15 +0300

    Routing works

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from blog.models import Post
+
+def index(request):
+    latest_blog_entry = Post.objects.order_by('-post_date')[:5]
+    context = {'latest_blog_entry': latest_blog_entry}
+    return render(request, 'index.html', context)
+
+def detail(requst, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(requst, 'detail.html', {'post': post})
